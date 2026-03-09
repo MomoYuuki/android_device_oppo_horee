ad60af772faa3d7ea0b6aecd320ef305be90a1d6
MomoYuuki <2711300270@qq.com>
2026-03-10 02:23:06 +0800

    horee: Update from PEDM00_14.0.0.2401(EX01)

--- extract-files.py
+++ extract-files.py
@@ -4,18 +4,67 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    BlobFixupCtx,
+    File,
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
 
 namespace_imports = [
+    'hardware/oplus',
     'vendor/oneplus/sm8250-common',
+    'vendor/qcom/opensource/display',
 ]
+
+blob_fixups: blob_fixups_user_type = {
+    ('odm/lib64/libSuperRaw.so', 'odm/lib64/libPerfectColor.so'): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
+    'odm/lib64/vendor.oplus.hardware.urcc-V1-ndk_platform.so': blob_fixup()
+        .add_needed('libjsoncpp_shim.so'),
+    ('odm/lib64/libarcsoft_hta.so', 'odm/lib64/libarcsoft_qnnhtp.so'): blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open')
+        .clear_symbol_version('remote_register_buf_attr')
+        .clear_symbol_version('remote_register_buf'),
+    'odm/lib64/libarcsoft_portrait_super_night_raw.so': blob_fixup()
+        .clear_symbol_version('remote_handle64_close')
+        .clear_symbol_version('remote_handle64_invoke')
+        .clear_symbol_version('remote_handle64_open')
+        .clear_symbol_version('rpcmem_alloc')
+        .clear_symbol_version('rpcmem_to_fd')
+        .clear_symbol_version('rpcmem_free'),
+    'vendor/etc/libnfc-nci.conf': blob_fixup()
+        .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
+    'vendor/etc/libnfc-nxp.conf': blob_fixup()
+        .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
+        .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
+    'odm/bin/hw/vendor.oplus.hardware.charger-V3-service': blob_fixup()
+        .replace_needed('libosenseaidlhalclient.so', 'libosenseaidlhalclient_charge.so'),
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
+        .add_needed('libcamera_metadata_shim.so'),
+}  # fmt: skip
 
 module = ExtractUtilsModule(
     'horee',
     'oppo',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
 
